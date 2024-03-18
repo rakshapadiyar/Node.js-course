3d0048312796a64e7eadcd143fd6e0e69e22cdc7
Raksha Padiyar <rakshapadiyar100@gmail.com>
2024-03-18 16:23:51 +0530

    module(1) node.js basics done

---
+++ 1_node.js_basics/assignment.js
@@ -0,0 +1,5 @@
+const firstName = process.argv[2];
+const secName = process.argv[3];
+console.log(`My name is ${firstName} ${secName}`);
+//$ node assignment.js antony gonsalves
+//My name is antony gonsalves
